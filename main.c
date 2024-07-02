1cb3e079c0048acb2be96dc0ad17ad8fad480e1c
undergnd <62897654+undergnd@users.noreply.github.com>
2024-07-02 13:49:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main(void)
 void system_init(void); // some general system initialization function
 	
 COM_Init(NULL);
+	
+#if DEBUG_ON == 1
 
   uint32_t UID_0 = LL_GetUID_Word0();
   uint32_t UID_1 = LL_GetUID_Word1();
@@ -12,7 +14,8 @@ COM_Init(NULL);
   printf("HELLO from Bluenrg-LP ");
   printf("UID_1: %x, UID_0: %x \r\n", UID_1, UID_0);
   LL_mDelay(10);
-
+#endif
+	
   rekrut_init();
   uint32_t counter_val_get = 0;
 
