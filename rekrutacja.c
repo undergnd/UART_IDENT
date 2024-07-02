0e03c02aab2da34ba413571262fcbccc0b7e7413
undergnd <62897654+undergnd@users.noreply.github.com>
2024-07-02 13:37:19 +0200

    Update rekrutacja.c

--- rekrutacja.c
+++ rekrutacja.c
@@ -24,21 +24,21 @@
 #define TIMx                       TIM1
 #define LL_EnableClock_TIMx()      LL_APB0_EnableClock(LL_APB0_PERIPH_TIM1);
 
-#define MODULE1						(0xA3)
-#define MODULE2						(0xA5)
-#define MODULE3						(0xA8)
-#define ANSWER_SUPPORTED			(0xAA)
-#define ANSWER_NOT_SUPPORTED		(0xAf)
+#define MODULE1			(0xA3)
+#define MODULE2			(0xA5)
+#define MODULE3			(0xA8)
+#define ANSWER_SUPPORTED	(0xAA)
+#define ANSWER_NOT_SUPPORTED	(0xAf)
 
 #define IRQ_EDGE_IS_SET_RISING	1
 #define IRQ_EDGE_IS_SET_FALLING	0
 
 #define K			(1) // number of bits taken to measure speed (1 or 2)
-#define S_115200 	(K*283) //K times TIM1 CNT value
-#define S_57600  	(K*552)
-#define S_38400     (K*830)
-#define S_19200   	(K*1664)
-#define S_9600 		(K*3330)
+#define S_115200 		(K*283) //K times TIM1 CNT value
+#define S_57600  		(K*552)
+#define S_38400     		(K*830)
+#define S_19200   		(K*1664)
+#define S_9600 			(K*3330)
 
 #define MAX_BIT_PERIOD 	3700
 #define PERC   			10 // percents of deviation
